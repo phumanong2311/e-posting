74baa01e4fb9cd2cd5a8ad6a9b30f52ec32f2200
phuma2311 <phumanong@gmail.com>
2023-12-16 00:05:26 +0700

    apply mantine core

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { MantineProvider, createTheme, rem } from "@mantine/core";
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import { router } from "./routes";
 
 function App() {
-  return <RouterProvider router={router} />;
+  const theme = createTheme({
+    colors: {
+      // Add your color
+      deepBlue: ["#E9EDFC", "#C1CCF6", "#99ABF0" /* ... */],
+      // or replace default theme color
+      blue: ["#E9EDFC", "#C1CCF6", "#99ABF0" /* ... */],
+    },
+
+    shadows: {
+      md: "1px 1px 3px rgba(0, 0, 0, .25)",
+      xl: "5px 5px 3px rgba(0, 0, 0, .25)",
+    },
+
+    headings: {
+      fontFamily: "Roboto, sans-serif",
+      sizes: {
+        h1: { fontSize: rem(36) },
+      },
+    },
+  });
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
+      <RouterProvider router={router} />
+    </MantineProvider>
+  );
 }
 
 export default App;
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,58 +1,33 @@
-import { Link } from "react-router-dom";
+import { Button, Group, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
 
 const LoginPage = () => {
+  const form = useForm({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+  });
   return (
-    <div className="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
-      <div className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
-        <div className="flex flex-col overflow-y-auto md:flex-row">
-          <main className="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
-            <div className="w-full">
-              <h1 className="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200">
-                ePosting Admin Portal
-              </h1>
-              <label>
-                <span>Email</span>
-                <input
-                  className="mt-1"
-                  type="email"
-                  placeholder="john@doe.com"
-                />
-              </label>
-
-              <label className="mt-4">
-                <span>Password</span>
-                <input
-                  className="mt-1"
-                  type="password"
-                  placeholder="***************"
-                />
-              </label>
-
-              <button className="mt-4" block to="/app">
-                Log in
-              </button>
-
-              <hr className="my-8" />
-
-              <p className="mt-4">
-                <Link
-                  className="text-sm font-medium text-purple-600 dark:text-purple-400 hover:underline"
-                  to="/forgot-password"
-                >
-                  Forgot your password?
-                </Link>
-              </p>
-              <p className="mt-1">
-                <Link
-                  className="text-sm font-medium text-purple-600 dark:text-purple-400 hover:underline"
-                  to="/create-account"
-                >
-                  Create account
-                </Link>
-              </p>
-            </div>
-          </main>
-        </div>
+    <div className="h-screen flex justify-center items-center">
+      <div>
+        <div className="text-purple-700 font-bold">ePosting Admin Portal</div>
+        <form onSubmit={form.onSubmit((values) => console.log(values))}>
+          <TextInput
+            label="Email"
+            placeholder="Email"
+            {...form.getInputProps("email")}
+          />
+          <TextInput
+            mt="md"
+            label="Email"
+            placeholder="Email"
+            {...form.getInputProps("email")}
+          />
+          <Group justify="center" mt="xl">
+            <Button type="submit">Submit</Button>
+          </Group>
+        </form>
       </div>
     </div>
   );
