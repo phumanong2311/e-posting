bfd72046a47f6883804e9d17e42230d7569516e4
phat-nv <1753082.17clc@gmail.com>
2023-12-16 16:06:47 +0700

    login page

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,5 +1,8 @@
-import { Button, Group, TextInput } from "@mantine/core";
+import { Button, Group, TextInput, Checkbox } from "@mantine/core";
 import { useForm } from "@mantine/form";
+
+import { IconEye, IconMail } from "@tabler/icons-react";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
   const form = useForm({
@@ -9,24 +12,63 @@ const LoginPage = () => {
     },
   });
   return (
-    <div className="h-screen flex justify-center items-center">
-      <div>
-        <div className="text-purple-700 font-bold">ePosting Admin Portal</div>
+    <div className="h-screen w-full flex justify-center items-center">
+      <div className="min-w-[800px]">
+        <div className="text-purple-700 font-bold text-3xl mb-[50px]">
+          ePosting Admin Portal
+        </div>
         <form onSubmit={form.onSubmit((values) => console.log(values))}>
           <TextInput
-            label="Email"
-            placeholder="Email"
+            label={<p className="text-lg text-black-300 mb-2">Email Address</p>}
+            placeholder="william@eposting.com"
+            className="w-full"
+            size="lg"
+            radius={10}
+            leftSection={<IconMail className="text-black" color="black" />}
             {...form.getInputProps("email")}
           />
+          <p className="text-sm text-gray-300 mt-2">
+            must use an email under the domain "@eposting.com"
+          </p>
           <TextInput
-            mt="md"
-            label="Email"
-            placeholder="Email"
-            {...form.getInputProps("email")}
+            label={<p className="text-lg text-black-300 mb-2">Password</p>}
+            placeholder="Password"
+            type="password"
+            className="w-full mt-5"
+            size="lg"
+            radius={10}
+            rightSection={<IconEye className="text-black" color="black" />}
+            {...form.getInputProps("password")}
           />
+          <p className="text-sm text-gray-300 mt-2">
+            this is a different email password than your ePosting google account
+            password
+          </p>
           <Group justify="center" mt="xl">
-            <Button type="submit">Submit</Button>
+            <Button
+              type="submit"
+              className="w-full bg-[#7167e8]"
+              size="lg"
+              radius={10}
+            >
+              Sign in
+            </Button>
           </Group>
+          <div className="mt-4 w-full flex justify-between items-center">
+            <Checkbox
+              label={
+                <p className="text-[16px] font-medium text-black">
+                  Remember me
+                </p>
+              }
+            />
+
+            <Link to="/forgot-password">
+              <p className="text-[16px] font-medium text-[#7e75ea]">
+                Forgot password?
+              </p>
+            </Link>
+          </div>
         </form>
       </div>
     </div>
